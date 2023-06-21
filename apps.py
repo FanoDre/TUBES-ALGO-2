03ef625bd2e30671701dcc30b2748ac50bb46463
FanoDre <fanodre@gmail.com>
2023-06-21 22:22:10 +0700

    TUBES FLASK

---
+++ apps.py
@@ -0,0 +1,29 @@
+# import os
+# from selenium import webdriver
+# from selenium.webdriver.common.keys import Keys
+# os.system('cls')
+
+# menu = [' Pertemuan 1',' Pertemuan 2',' Pertemuan 3',
+#         ' Pertemuan 4',' Pertemuan 5',' Pertemuan 6',
+#         ' Pertemuan 7',' Pertemuan 8',' Pertemuan 9',
+#         'Pertemuan 10','Pertemuan 11','Pertemuan 12',
+#         ]
+
+# # Start -------------------------------------
+# print(50*'=')
+# print("\t\tSelamat Datang")
+# print(50*'=')
+# print("\t   Aplikasi Materi Algoritma 2")
+# print(50*'-')
+
+# # Menu --------------------------------------
+# print("Menu:")
+# for i, menus in enumerate(menu):
+#   print(f"\t{i+1}. {menu[i]}")
+  
+# print(50*'-')
+
+# selectMenu = input("Pilih Menu: ")
+# if selectMenu == "1":
+#   driver = webdriver.Chrome('C:/Users/Admin/Downloads/chromedriver_win32/chromedriver.exe')
+#   driver.get("https://www.javatpoint.com/python-tutorial")     
